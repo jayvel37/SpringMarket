5e1c38dfd3b2f465dbfc19eeb171b59d8679f768
jayvel37 <Jayvelazco@gmail.com>
2024-02-19 01:05:54 -0700

    Inital github push

--- src/main/java/com/example/springmarket/SpringMarketApplication.java
+++ src/main/java/com/example/springmarket/SpringMarketApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMarketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMarketApplication.class, args);
+    }
 
 }
